0213d7096404b3234c67bcc811fb2afa0ea2b643
Furkan Arslan <furkan.arslan@konya.bel.tr>
2022-02-16 16:45:56 +0300

    odev tamamlandı

---
+++ odev.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+
+    for($i=1;$i<=5;$i++)
+    {
+        for($a=1;$a<=$i;$a++){
+            echo" F ";
+        }
+        echo"<br>";
+    }
+
+    /*
+    F
+    F F
+    F F F
+    F F F F
+    F F F F F
+    */
+
+?>
+</body>
+</html>
